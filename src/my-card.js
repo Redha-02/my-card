9c4e938eb1330ac0775b6c2b0b0a8431857ece4a
Redha <rfa5347@psu.edu>
2023-10-06 14:45:44 -0400

    editted meme-maker import

--- src/my-card.js
+++ src/my-card.js
@@ -1,5 +1,5 @@
 import { LitElement, html, css } from 'lit';
-import "@lrnwebcomponents/meme-maker";
+import "@lrnwebcomponents/meme-maker/meme-maker.js";
 
 export class MyCard extends LitElement {
     static properties = {
